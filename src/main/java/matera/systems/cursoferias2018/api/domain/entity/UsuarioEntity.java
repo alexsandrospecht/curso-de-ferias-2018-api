394f40aa9a0ea2ac80aea7fba79f65be8beade4c
rochapaulo <almeida.paulorocha@gmail.com>
2017-10-02 16:40:29 -0300

    tests

--- src/main/java/matera/systems/cursoferias2018/api/domain/entity/UsuarioEntity.java
+++ src/main/java/matera/systems/cursoferias2018/api/domain/entity/UsuarioEntity.java
@@ -1,4 +1,70 @@
 package matera.systems.cursoferias2018.api.domain.entity;
 
+import java.util.UUID;
+
 public class UsuarioEntity {
+
+    private UUID uuid;
+    private String nome;
+    private String perfil;
+    private String email;
+    private String login;
+    private String senha;
+    private String urlPhoto;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPerfil() {
+        return perfil;
+    }
+
+    public void setPerfil(String perfil) {
+        this.perfil = perfil;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getUrlPhoto() {
+        return urlPhoto;
+    }
+
+    public void setUrlPhoto(String urlPhoto) {
+        this.urlPhoto = urlPhoto;
+    }
 }
---
+++ src/main/java/matera/systems/cursoferias2018/api/repository/UsuarioRepositoryStub.java
@@ -0,0 +1,66 @@
+package matera.systems.cursoferias2018.api.repository;
+
+import matera.systems.cursoferias2018.api.domain.entity.UsuarioEntity;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@Profile("stub")
+public class UsuarioRepositoryStub implements UsuarioRepository {
+
+    public static final UUID USUARIO_1 = UUID.fromString("3480ed0e-2c8d-4a69-a8ed-7a2f136c4c20");
+    public static final UUID USUARIO_2 = UUID.fromString("bc51c8bb-bad3-47e4-af0c-7f467148f23d");
+    public static final UUID USUARIO_3 = UUID.fromString("4a8975d1-9e37-4872-bd35-1050707484f8");
+
+    private static final Map<UUID, UsuarioEntity> data = new HashMap<>();
+
+    static {
+        {
+            UsuarioEntity entity = new UsuarioEntity();
+            entity.setUuid(USUARIO_1);
+            data.put(USUARIO_1, entity);
+        }
+
+        {
+            UsuarioEntity entity = new UsuarioEntity();
+            entity.setUuid(USUARIO_2);
+            data.put(USUARIO_2, entity);
+        }
+
+        {
+            UsuarioEntity entity = new UsuarioEntity();
+            entity.setUuid(USUARIO_3);
+            data.put(USUARIO_3, entity);
+        }
+    }
+
+    @Override
+    public UUID criar(UsuarioEntity usuario) {
+        UUID uuid = UUID.randomUUID();
+        data.put(uuid, usuario);
+        return uuid;
+    }
+
+    @Override
+    public void deletar(UUID uuid) {
+        data.remove(uuid);
+    }
+
+    @Override
+    public List<UsuarioEntity> listar() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public Optional<UsuarioEntity> findByID(UUID uuid) {
+        return Optional.ofNullable(data.getOrDefault(uuid, null));
+    }
+
+    @Override
+    public void atualizar(UsuarioEntity update) {
+        data.put(update.getUuid(), update);
+    }
+
+}
