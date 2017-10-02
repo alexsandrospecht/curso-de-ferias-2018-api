ca4b491e6f8cfdb6a099864b4f760d7a2950e0f1
Junior Miqueletti <juniormiqueletti@gmail.com>
2017-10-02 14:20:18 -0300

    Implementando oauth

--- src/main/java/matera/systems/cursoferias2018/api/domain/request/UsuarioLoginRequest.java
+++ src/main/java/matera/systems/cursoferias2018/api/domain/request/UsuarioLoginRequest.java
@@ -1,4 +1,23 @@
 package matera.systems.cursoferias2018.api.domain.request;
 
 public class UsuarioLoginRequest {
+
+    private String nome;
+    private String senha;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
 }
--- src/main/java/matera/systems/cursoferias2018/api/domain/response/UsuarioLoginResponse.java
+++ src/main/java/matera/systems/cursoferias2018/api/domain/response/UsuarioLoginResponse.java
@@ -1,4 +1,19 @@
 package matera.systems.cursoferias2018.api.domain.response;
 
 public class UsuarioLoginResponse {
+
+    private UsuarioResponse usuario;
+    private String token;
+
+    public UsuarioResponse getUsuario() {
+        return usuario;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
