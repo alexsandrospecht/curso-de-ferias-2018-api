59ab68837a4d83e75a6622c765b2ba199ac0bf7d
Junior Miqueletti <juniormiqueletti@gmail.com>
2017-10-02 14:18:09 -0300

    Implementando oauth

---
+++ src/main/java/matera/systems/cursoferias2018/api/conf/security/AuthorizationServerConfig.java
@@ -0,0 +1,40 @@
+package matera.systems.cursoferias2018.api.conf.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+            .withClient("angular")
+            .secret("@ngul@r")
+            .scopes("read","write")
+            .authorizedGrantTypes("password")
+        .and()
+            .withClient("mobile")
+            .secret("alunos")
+            .scopes("read","write")
+            .authorizedGrantTypes("password")
+        .accessTokenValiditySeconds(14400);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .tokenStore(new InMemoryTokenStore())
+                .authenticationManager(authenticationManager);
+    }
+}
